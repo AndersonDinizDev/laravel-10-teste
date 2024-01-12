47d85b1c581664e91686bf50a471edb0e4db5450
andersondinizdev <andersondiniz159@gmail.com>
2024-01-12 09:41:37 -0300

    Atualizando listagem.

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -35,4 +35,29 @@ class SupportController extends Controller
 
         return redirect()->route('supports.index');
     }
+
+    public function edit(Support $support, string|int $id) {
+        if(!$support = $support->where('id', $id)->first()) {
+            return back();
+        }
+
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string $id) {
+        if(!$support = Support::find($id)) {
+            return back();
+        }
+
+        // $support->subject = $request->subject;
+        // $support->subject = $request->body;
+        // $support->save();
+
+        $support->update($request->only([
+            'subject',
+            'body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
 }
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,5 +1,6 @@
 <h1>Listagem dos suportes</h1>
 
+<a href="{{ route('supports.create') }}">Criar dúvida</a>
 <table>
     <thead>
         <th>assunto</th>
@@ -15,6 +16,7 @@
         <td>{{ $support->body }}</td>
         <td>
             <a href="{{ route('supports.show', $support->id) }}">Ir</a>
+            <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
         </td>
     </tr>
         @endforeach
