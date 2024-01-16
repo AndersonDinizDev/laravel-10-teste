3e6a59c65a1e01d6b979546547e3c3459ad679d4
andersondinizdev <andersondiniz159@gmail.com>
2024-01-16 07:58:16 -0300

    laravel apirest

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,6 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/contact', [SiteController::class, 'contact']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
